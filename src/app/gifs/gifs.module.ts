1d57c9843706c6ba4526726045ff1dd10fea90de
jomas94 <jomariojsa@gmail.com>
2021-06-10 14:55:20 +0200

    component busqueda

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 
 
 @NgModule({
   declarations: [
-    GifsPageComponent
+    GifsPageComponent,
+    BusquedaComponent
   ],
   exports:[
     GifsPageComponent
